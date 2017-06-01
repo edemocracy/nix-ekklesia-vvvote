e9415fa672a1c51e3fd6f78e973bf728b3782cf0
dpausp <dpausp@posteo.de>
2017-06-01 15:23:09 +0000

    factor out vvvote backend expression

--- default.nix
+++ default.nix
@@ -9,51 +9,7 @@ lib = pkgs.lib;
 vars = if vars_override != null then vars_override
   else lib.recursiveUpdate (import ./default_vars.nix) (scopedImport { inherit pkgs lib; } customVarsPath);
 
-thisConfig = scopedImport { inherit vars; } ./conf-thisserver_template.php.nix;
-allConfig = scopedImport { inherit vars; } ./conf-allservers_template.php.nix;
-
-thisConfigFile = pkgs.writeText "conf-thisserver.php" thisConfig;
-allConfigFile = pkgs.writeText "conf-allservers.php" allConfig;
-keydir = toString vars.keydir;
-
-publicKeyFiles = if (vars.keydir == null) then [] else
-  map (i: "${keydir}/PermissionServer${toString i}.publickey") (lib.range 1 vars.number_of_servers);
-
-
-vvvoteBackend = pkgs.stdenv.mkDerivation {
-  name = "vvvote";
-  src = pkgs.fetchFromGitHub {
-    owner = "pfefffer";
-    repo = "vvvote";
-    rev = "e63ab761eb738455216cfe3d386eaa75504e1e3b";
-    sha256 = "0la05r2lkgs9dips2c3fj6wdlqphjjnvj9f5ld22lhgcji7kvf1p";
-  };
-
-  propagatedBuildInputs = [];
-
-  dontBuild = true;
-  installPhase = ''
-    set -x
-    config_dir=$out/config
-    cp -r $src/backend $out
-    chmod u+w -R $out
-    # linking doesn't work because PHP uses the location of the real file for __DIR__
-    cp ${thisConfigFile} $config_dir/conf-thisserver.php
-    cp ${allConfigFile} $config_dir/conf-allservers.php
-    rm -f $config_dir/conf*example*.php
-  '' 
-  + lib.optionalString (vars.keydir != null) ''
-    # link public permission server keys (optional: pass them as argument?)
-    ${lib.concatMapStringsSep "\n" (k: "ln -s ${k} $config_dir") publicKeyFiles}
-    # link private keys
-    ln -s ${keydir}/PermissionServer${toString vars.server_number}.privatekey.pem.php $config_dir
-    set +x
-  '' 
-  + lib.optionalString (vars.keydir != null && vars.is_tally_server) ''
-    ln -s ${keydir}/TallyServer${toString vars.server_number}.privatekey.pem.php $config_dir
-    ln -s ${keydir}/TallyServer${toString vars.server_number}.publickey $config_dir
-  '';
-};
+vvvoteBackend = pkgs.callPackage ./vvvote_backend.nix { inherit vars; };
 
 uwsgiConfig = pkgs.writeText "vvvote-uwsgi-config.ini" ''
   [uwsgi]
